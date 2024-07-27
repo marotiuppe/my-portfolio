86f763442e2ff14d119bb1d755431942f57aa6c4
marotiuppe <marotiuppe891@gmail.com>
2024-07-27 19:46:47 +0530

    commented secret keys

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,7 +9,7 @@ export class DataService {
   private apiUrl = 'https://api.example.com'; // Replace with your API URL
   private gitRepUrl = 'https://api.github.com/users/marotiuppe/repos';
   // private gitAuthorization = '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';
-  private gitAuthorization = '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';
+  // private gitAuthorization = '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';
   constructor(private http: HttpClient) {}
 
   
@@ -26,14 +26,14 @@ export class DataService {
   getAllGitReposData(): Observable<any> {
     let headers = new HttpHeaders({
       'Content-Type': 'application/json', 
-      'Authorization': this.gitAuthorization
+      // 'Authorization': this.gitAuthorization
     });
     return this.http.get<any>(this.gitRepUrl,{ headers });
   }
   getDataByUrl(url:any): Observable<any> {
     let headers = new HttpHeaders({
       'Content-Type': 'application/json', 
-      'Authorization': this.gitAuthorization
+      // 'Authorization': this.gitAuthorization
     });
     return this.http.get<any>(url,{headers});
   }
